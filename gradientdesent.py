dde7e89c0d2d6977f3f3596f357951bc336b20f6
Alireza Salimi <alirezasalimi382@gmail.co>
2018-12-03 20:52:29 +0330

    add visualzation 01

--- gradientdesent.py
+++ gradientdesent.py
@@ -28,33 +28,29 @@ class gradientDesent():
 
 
         """
-
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111)
-        self.x = np.array([1 ,2,3,4])
-        self.y =  np.array([2,4,6,8])
         pass
+
 
 
     def gradientDisent(self):
 
         x = self.x #  x as regressor
         y =self.y  #  y as pridictor
-       
-        theata_1 = 2                 
+
+        theata_1 = 2
         theata_0 = 4
         alpha = 0.01
-        itrator = 10000   
+        itrator = 10000
         n = len(x)
         for i in range(itrator):
-            model = theata_0 + theata_1*x
+            model = theata_0 + theata_1 * x
 
-            cb = (1/n)*alpha* np.sum(model - y)  # derivitive of cost fumction for thetha_0
-            cm = (1/n)* alpha * np.sum((model -y)*x)# derivitive of cost fumction for thetha_1
-           
+            cb = (1 / n) * alpha * np.sum(model - y)  # derivitive of cost fumction for thetha_0
+            cm = (1 / n) * alpha * np.sum((model - y) * x)  # derivitive of cost fumction for thetha_1
+
             theata_0 = theata_0 - cb
             theata_1 = theata_1 - cm
-            
+
         print(theata_0,theata_1)
 
 
